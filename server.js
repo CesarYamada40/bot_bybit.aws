4f3db84e819390065ea28099b09eb183205a41d8
Cesar Yamada <122217005+CesarYamada40@users.noreply.github.com>
2025-10-24 00:48:50 -0300

    Change server port from 5000 to 10000

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.get('/bybit-auth-debug', async (req, res) => {
 });
 
 // Start server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 10000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
